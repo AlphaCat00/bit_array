70edcb7e8e5b9dd1033369ab39ac4f311aeff86e
AlphaCat00 <00.alpha.cat@gmail.com>
2022-11-09 00:10:37 -0800

    add bitwise operations based implementation

--- bit_array.h
+++ bit_array.h
@@ -3,12 +3,126 @@
 #include <stdint.h>
 
 #include <type_traits>
-template <uint64_t bits, uint64_t size, class Enable = void>
-class bit_array_base;
 
+namespace compact_bit_array {
+
+// bitfield based implementation
+template <uint64_t bits, uint64_t size, class Enable = void>
+class impl_bitfield;
+
+// 000
 template <uint64_t bits, uint64_t size>
-    class bit_array_base < bits,
-    size, typename std::enable_if<bits<64>::type> {
+    class impl_bitfield < bits,
+    size, typename std::enable_if<!(bits & 7) && bits<64>::type> {
+   protected:
+    constexpr static uint64_t _shift = 0;
+    constexpr static uint64_t _mask = (1 << _shift) - 1;
+    struct _buf_t {
+        uint64_t _0 : bits;
+    } __attribute__((__packed__));
+    uint8_t _buf[(size * bits + 7) >> 3];
+    inline _buf_t &_get_buf(uint64_t index) {
+        return reinterpret_cast<_buf_t *>(_buf)[index >> _shift];
+    };
+
+   public:
+    inline uint64_t get(uint64_t index) {
+        return _get_buf(index)._0;
+    }
+
+    inline void set(uint64_t index, uint64_t val) {
+        _get_buf(index)._0 = val;
+    }
+};
+
+// 100
+template <uint64_t bits, uint64_t size>
+    class impl_bitfield < bits,
+    size, typename std::enable_if<!(bits & 3) && (bits & 4) && bits<64>::type> {
+   protected:
+    constexpr static uint64_t _shift = 1;
+    constexpr static uint64_t _mask = (1 << _shift) - 1;
+    struct _buf_t {
+        uint64_t _0 : bits;
+        uint64_t _1 : bits;
+    } __attribute__((__packed__));
+    uint8_t _buf[(size * bits + 7) >> 3];
+    inline _buf_t &_get_buf(uint64_t index) {
+        return reinterpret_cast<_buf_t *>(_buf)[index >> _shift];
+    };
+
+   public:
+    inline uint64_t get(uint64_t index) {
+        switch (index & _mask) {
+            case 0:
+                return _get_buf(index)._0;
+            case 1:
+                return _get_buf(index)._1;
+        }
+        return 0;
+    }
+
+    inline void set(uint64_t index, uint64_t val) {
+        switch (index & _mask) {
+            case 0:
+                _get_buf(index)._0 = val;
+            case 1:
+                _get_buf(index)._1 = val;
+        }
+    }
+};
+
+// X10
+template <uint64_t bits, uint64_t size>
+    class impl_bitfield < bits,
+    size, typename std::enable_if<!(bits & 1) && (bits & 2) && bits<64>::type> {
+   protected:
+    constexpr static uint64_t _shift = 2;
+    constexpr static uint64_t _mask = (1 << _shift) - 1;
+    struct _buf_t {
+        uint64_t _0 : bits;
+        uint64_t _1 : bits;
+        uint64_t _2 : bits;
+        uint64_t _3 : bits;
+    } __attribute__((__packed__));
+    uint8_t _buf[(size * bits + 7) >> 3];
+    inline _buf_t &_get_buf(uint64_t index) {
+        return reinterpret_cast<_buf_t *>(_buf)[index >> _shift];
+    };
+
+   public:
+    inline uint64_t get(uint64_t index) {
+        switch (index & _mask) {
+            case 0:
+                return _get_buf(index)._0;
+            case 1:
+                return _get_buf(index)._1;
+            case 2:
+                return _get_buf(index)._2;
+            case 3:
+                return _get_buf(index)._3;
+        }
+        return 0;
+    }
+
+    inline void set(uint64_t index, uint64_t val) {
+        switch (index & _mask) {
+            case 0:
+                _get_buf(index)._0 = val;
+            case 1:
+                _get_buf(index)._1 = val;
+            case 2:
+                _get_buf(index)._2 = val;
+            case 3:
+                _get_buf(index)._3 = val;
+        }
+    }
+};
+
+// XX1
+template <uint64_t bits, uint64_t size>
+    class impl_bitfield < bits,
+    size, typename std::enable_if<(bits & 1) && bits<64>::type> {
    protected:
     constexpr static uint64_t _shift = 3;
     constexpr static uint64_t _mask = (1 << _shift) - 1;
@@ -72,8 +186,47 @@ template <uint64_t bits, uint64_t size>
     }
 };
 
+// bitwise operations based implementation
+template <uint64_t bits, uint64_t size, class Enable = void>
+class impl_bitwise;
+
 template <uint64_t bits, uint64_t size>
-class bit_array : public bit_array_base<bits, size> {
+class impl_bitwise<bits,
+                     size, typename std::enable_if<bits <= 57>::type> {
+   protected:
+    using T = typename std::conditional<bits <= 25, typename std::conditional<bits <= 9, uint16_t, uint32_t>::type, uint64_t>::type;
+    constexpr static uint64_t _byte_shift = 3;
+    constexpr static uint64_t _byte_mask = (1 << _byte_shift) - 1;
+
+    constexpr static T _mask = (1 << bits) - 1;
+    uint8_t _buf[(size * bits + _byte_mask) >> _byte_shift];
+    inline T &_get_buf(uint64_t index) {
+        return *reinterpret_cast<T *>(_buf + (index * bits >> _byte_shift));
+    };
+    inline T _get_shift(uint64_t index) {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        return index * bits & _byte_mask;
+#else
+        return (sizeof(T) << _byte_shift) - bits - (index * bits & _byte_mask);
+#endif
+    };
+
+   public:
+    inline uint64_t get(uint64_t index) {
+        return (_get_buf(index) >> _get_shift(index)) & _mask;
+    }
+
+    inline void set(uint64_t index, uint64_t val) {
+        T &buf = _get_buf(index);
+        T shift = _get_shift(index);
+        buf &= ~(_mask << shift);
+        buf |= (val & _mask) << shift;
+    }
+};
+
+// bit_array interface
+template <uint64_t bits, uint64_t size, typename impl = impl_bitfield<bits, size>>
+class bit_array : public impl{
    public:
     class reference {
         bit_array *base;
@@ -96,4 +249,5 @@ class bit_array : public bit_array_base<bits, size> {
     }
 };
 
+}  // namespace compact_bit_array
 #endif
--- readme.md
+++ readme.md
@@ -9,6 +9,7 @@ A compact C++ array whose elements' length can be arbitrary **bits** (<64).
     #include "bit_array.h"
 
     int main() {
+        using namespace compact_bit_array;
         bit_array<6, 4> a;
         bit_array<3, 4> b;
         b[0] = a[0] = 22;
@@ -18,6 +19,13 @@ A compact C++ array whose elements' length can be arbitrary **bits** (<64).
         printf("%ld\n", sizeof(bit_array<6, 4>));
         printf("%ld\n", sizeof(bit_array<8, 4>));
         printf("%ld\n", sizeof(bit_array<16, 4>));
+        
+        bit_array<5, 500, impl_bitwise<5,500>> test;
+        printf("%ld\n", sizeof(test));
+        for (uint64_t i = 0; i < 500; i++)
+            test.set(i, i);
+        for (uint64_t i = 0; i < 500; i++)
+            printf("%ld\n", test.get(i));
         return 0;
     }
 
